764214b296c3cc561e1754793b9973190cb2c233
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-17 16:58:52 +0530

    VOWEL.C
    PROGRAM TO FIND VOWEL OR CONSONANT

---
+++ VOWEL.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int vowel1,vowel2;
+printf("enter an alphabet:");
+scanf("%c",&c);
+vowel1=(c=='a','e','i','o','u');
+vowel2=(c=='A','E','I','O','U');
+if(vowel=vowel2)
+{
+printf("%c is a vowel");
+}
+else
+{
+printf("%c is a consonant");
+}
+getch();
+};
