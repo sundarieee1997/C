33f96c950a4bf91ce4697430c7a0f9ae48abb092
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-17 16:35:26 +0530

    EVEN.C
    PROGRAM TO FIND A NUMBER IS EVEN OR ODD

---
+++ EVEN.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int var:
+printf("enter a number");
+scanf("%d",&var);
+if(var %2==0)
+{
+printf("you have entered even number");
+}
+else
+{
+printf("you have entered a odd number");
+}
+getch();
+};
