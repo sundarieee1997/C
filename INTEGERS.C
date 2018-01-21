b6f6ea57372f845b58606c9fd3ec0c56b04db4c9
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-21 16:31:12 +0530

    INTEGERS.C
    program to find the sum of first integer in two numbers

---
+++ INTEGERS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,l;
+printf("enter a two numbers:");
+scanf("%d%d",&i,&j);
+for(i=0;i<=k;i++)
+{
+printf("%d\n",i);
+}
+for(j=0;j<=i;j++0
+{
+l+=j;
+}
+getch();
+}
