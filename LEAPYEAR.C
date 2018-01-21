c706bf04b041268d8fc6f0339970d7cb42554bdc
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-21 15:46:32 +0530

    LEAPYEAR.C
    program to find whether the given year is leap or not

---
+++ LEAPYEAR.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int year;
+printf("enter a year:");
+scanf("%d",&year);
+if year(%400==0)
+{
+printf("%d is a leap year",year);
+}
+else
+{
+printf("%d is a not leap year",year);
+}
+getch();
+}
