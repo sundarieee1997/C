676cf93dc5249c887602f51d50cd7d8fe97f2e40
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-31 15:55:08 +0530

    POWER.C
    program to find the power of the number

---
+++ POWER.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int base,exponent;
+long long power=1;
+int i;
+printf("enter base:");
+scanf("%d",&base);
+printf("enter exponent:");
+scanf("%d",&exponent);
+for(i=1;i<=exponent;i++)
+{
+power=power*base;
+}
+printf("%d^%d=%lld",base,exponent,power);
+return(0);
+}
