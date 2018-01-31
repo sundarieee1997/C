5e6a07364834fdb8cd572a01b2a54ba866fda200
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-31 16:59:02 +0530

    MULTIPLY.C
    PROGRAM TO FIND THE MULTIPLY NUMBER

---
+++ MULTIPLY.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+longint sum=0,i,n;
+printf("\n please give the value of N:");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+printf("\n 5* %ld=%ld",5*i);
+}
+return(0);
+}
