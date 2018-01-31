082c1290ef874d1bd62f40f98999e04299d28610
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-01-31 16:50:48 +0530

    ARMSTRONG NUMBER.C
    program to display the armstrong number between 2 intervals

---
+++ ARMSTRONG NUMBER.C
@@ -0,0 +1,31 @@
+#inclufde<stdio.h>
+int main()
+{
+int low,high,i,temp1,temp2,remainder,n=0,result=0;
+printf("enter the two numbers (intervals):");
+scanf("%d %d",&low,&high);
+printf("armstrong numbers between %d an %d are:",low,high);
+for(i=low+1;i<high;++i)
+{
+temp2=i;
+temp1=1;
+while(temp1 !=0)
+{
+temp1 /=10;
+++n;
+}
+while(temp2!=0)
+{
+remainder=temp2%10;
+result+=pow(remainder,n);
+temp2 /=10;
+}
+if(result==i)
+{
+printf("%d",i);
+}
+n=0;
+result=0;
+}
+return(0);
+}
