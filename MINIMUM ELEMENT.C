b9deba0287d8771972aa33a583db78a088ad586d
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-02 16:12:59 +0530

    MINIMUM ELEMENT.C
    program to find the smallest number in array

---
+++ MINIMUM ELEMENT.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int array[50],size,i,small;
+printf("\n enter the size of the array:");
+scanf("%d",&size);
+printf("\n enter %d elements of the array:",size);
+scanf("%d",&array[i]);
+for(i=0;i<size;i++)
+{
+small=array[i];
+}
+for(i=1;i<size;i++)
+{
+if(small>array[i])
+{
+small=array[i];
+}
+printf("\n small element in array is:%d",small);
+return(0);
+}
