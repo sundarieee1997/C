3e647a27d5a99f269e61fa270408e252902b3a93
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-05 16:35:32 +0530

    SUBTRACTION5.2.C
    program to sub the given two times

---
+++ SUBTRACTION5.2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int time1,time2,c;
+printf("enter the time1:");
+scanf("%d",&time1);
+printf("enter the time2:");
+scanf("%d",&time2);
+c=time1-time2;
+printf("the result is:",c);
+return(0);
+}
