c0c008b1c91c4eb08480e9c72a7f03bc6d499330
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-09 10:41:02 +0000

    STRING9.2.C
    program to print the string

---
+++ STRING9.2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i;
+char a[30];
+printf("enter the string:");
+scanf("%d",&a);
+printf("enter the no of tyms:");
+scanf("%d",&i);
+for(i=0;i>=a;i++)
+{
+printf("%d",i);
+}
+return(0);
+}
