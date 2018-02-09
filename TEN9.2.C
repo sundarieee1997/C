04dca8cbb1c8b89a506eb39ecd5bc5860621835f
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-09 10:21:22 +0000

    TEN9.2.C
    program to find the maximum number among 10 numbers

---
+++ TEN9.2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a[10];
+int i;
+in greatest;
+printf("enter ten values:");
+scanf("%d",&a[i]);
+for(i=0;i<10;i++)
+{
+greatest=a[0];
+}
+for(i=0;i<10;i++)
+{
+if(a[i]>greatest)
+{
+greatest=a[i];
+}
+}
+printf("greatest of ten numbers is %d",greatest);
+return(0);
+}
