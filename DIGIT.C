b32d0ec7599fdf92cd6eb8a57fac4a2c6be1c042
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-21 15:55:33 +0530

    DIGIT.C
    program to print the sum of the numbers

--- DIGIT.C
+++ DIGIT.C
@@ -4,8 +4,8 @@ int main()
 	int a,b,c,s;
 	printf("enter a three numbers: ");
 	scanf("%d %d %d",&a,&b,&c);
-	s=a+b+c;
-	printf("\n the sumof all digit is: %d");
+	s=(a+b+c);
+	printf("\n the sumof all digit is: %d",s);
 	return(0);
 	
 }
