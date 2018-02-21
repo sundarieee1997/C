11d1cfe32a06bcd054a2a8342155f3d523a04e47
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-21 16:19:25 +0530

    PRINT EVEN.C
    program to print the even number

---
+++ PRINT EVEN.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int a,n;
+	printf("enter a number");
+	scanf("%d",&n);
+if(n %2==0)
+{
+	a=n+1;
+	printf("num",a);
+}
+else
+{
+		a=n-1;
+	printf("num",a);
+
+}
+return(0);
+}
