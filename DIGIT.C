7322a00b6441de031efe3a3c4e8796d6430345d6
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-21 15:53:01 +0530

    DIGIT.C
    program to find the sum of the numbers

---
+++ DIGIT.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c,s;
+	printf("enter a three numbers: ");
+	scanf("%d %d %d",&a,&b,&c);
+	s=a+b+c;
+	printf("\n the sumof all digit is: %d");
+	return(0);
+	
+}
