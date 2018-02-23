d1c419cc0a81631174e383e53b14d7aae917cc2e
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-23 16:52:32 +0530

    10.C
    program to print the multiple of 10

---
+++ 10.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a!=20)
+	{
+		printf("\n the greatest multiple is 10");
+	}
+	else if(a!=10)
+	{
+		printf("\n the greatest multiple is 20");
+	}
+	return(0);
+}
