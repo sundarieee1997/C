3fd8d9bf7edcc224fa3209fba62f3ab1e68b53aa
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-02-23 16:19:32 +0530

    WN.C
    program to find the number of words in the string

---
+++ WN.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int count_words=0,i;
+	char str[20];
+	printf("enter the string");
+	scanf("%s",str);
+	if(str[i]=='str')
+	{
+		count_words++;
+	}
+	printf("\n number of words in string :%d",count_words);
+	return(0);
+}
