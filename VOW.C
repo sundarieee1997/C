2c9070732e475586d3ce18b3cfae92cc57f966c0
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-02 16:02:54 +0530

    VOW.C
    program to check vowel or not

---
+++ VOW.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int v1,v2;
+printf("enter an alphabet:");
+scanf("%c",&c);
+v1=(c==('a''e''i''o''u'));
+v2=(c==('A''E''I''O''U'));
+if(vowel1=vowel2)
+{
+printf("%c is a vowel");
+}
+return(0);
