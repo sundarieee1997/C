e5136d153ae10ed7a37266d72380b6f9ac50f880
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-05 16:56:45 +0530

    LEXICOGRAPHICAL.C
    program to find lexicographical

---
+++ LEXICOGRAPHICAL.C
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+#include<string.h>
+int main() 
+{
+	int i,j,temp=0;
+	char s1[100];
+	printf("enter the string");
+	gets(s1);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		for(j=i+1;s1[j]!='\0';j++)
+		{
+			if(s1[i]>s1[j])
+			{
+				temp=s1[i];
+				s1[i]=s1[j];
+				s1[j]=temp;
+			}
+		}
+	}
+	printf("\n lexicographical:%s",temp);
+	return(0);
+}
