b12015b7d4537ca8fd69b6f134b7c9d2fd3dd1a7
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-05 15:44:16 +0530

    OC.C
    program to print odd number in the given number

---
+++ OC.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the number");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		b=a%10;
+		if(b%2!=0)
+		{
+			printf("%d",b);
+		}
+		a=a/10;
+	}
+	return(0);
+}
