1120920c16e8aa906342c86075314eeecc06b534
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-07 15:50:28 +0530

    NUMERIC.C
    program to find the numerical

---
+++ NUMERIC.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char a[30];
+    int n;
+    printf("enter the string");
+    scanf("%s %d",&a,&n);
+   printf("\n the numeric value is:%d",n);
+     return(0);
+}
