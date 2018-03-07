e33c43df5766ee50018dce5592c94f9599021e62
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-07 15:56:05 +0530

    CUBOID.C
    program to find the cuboid length ,height and width

---
+++ CUBOID.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c,d;
+   printf("enter the len bret and wid:");
+   scanf("%d %d %d",&a,&b,&c);
+   d=(a*b*c);
+   printf("\n the cuboid is:%d",d);
+     return(0);
+}
+
