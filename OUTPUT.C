cf6d6c742a650809a178819f67176146ad620e02
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-09 16:15:45 +0530

    OUTPUT.C
    program to find the number

---
+++ OUTPUT.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter the three numbers");
+	scanf("%d %d %d",&a,&b,&c);
+	d=(a*b)/c;
+	printf("\n the output is:%d",d);
+	
+	return(0);
+}
