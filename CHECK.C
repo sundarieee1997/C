01954b15f73ef7d702eec74191c91bf2389ded93
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-09 16:50:08 +0530

    CHECK.C
    program to check two numbers are equal

---
+++ CHECK.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the two numbers");
+scanf("%d %d",&a,&b);
+if(a==b)
+{
+	printf("\n yes");
+}
+else 
+{
+printf("\n no");	
+}
+return(0);
+}
