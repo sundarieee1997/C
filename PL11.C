9601ee2e20ccdb0ed2b7b7c4643be427d6e48085
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-14 16:57:14 +0530

    PL11.C
    program to check the holiday or not

---
+++ PL11.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	char a[500];
+	int b;
+	printf("enter the day");
+	scanf("%s",&a);
+printf("\n enter the option");
+scanf("%d",&b);
+switch(b)
+{
+	case 1:
+	printf("\n yes");
+	break;
+	case 2:
+	printf("\n no");
+	break;
+	case 3:
+	printf("\n no");
+	break;
+	case 4:
+	printf("\n no");
+	break;
+	case 5:
+	printf("\n no");
+	break;
+	case 6:
+	printf("\n no");
+	break;
+	case 7:
+	printf("\n yes");
+	break;
+	
+}
+return(0);
+}
