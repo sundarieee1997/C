cc8faa9d43b348f5aae622742743fc2902b29387
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-19 16:24:11 +0530

    PL23.C
    program to find the maximum number

---
+++ PL23.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() 
+{
+int a[50];
+int k,i=0,p,n,j,max=0;
+printf("enter the numbers");
+scanf("%d %d",&n,&p);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+j=0;
+while(j<p)
+{
+scanf("%d",&k);
+a[n]=k;
+n++;
+for(i=0;i<n;i++)
+{
+ if(a[i]>=max)
+{
+ max=a[i];
+ }
+ }
+ printf("%d ",max); 
+j++; 
+}
+return 0;
+}
