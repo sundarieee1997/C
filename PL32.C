9838c1320c85afb7aaf7f4b61a0877b09d6c23df
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-21 16:25:58 +0530

    PL32.C
    program to check

---
+++ PL32.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the 1st number");
+scanf("%d%d",&a);
+printf("\n enter the 2nd number");
+scanf("%d%d",&b);
+if(a==b)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
