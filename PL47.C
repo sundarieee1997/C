1787a78c11d6fb76bf244f3bbe199f82eccedd0a
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-26 16:33:56 +0530

    PL47.C
    program to check the angles

--- PL47.C
+++ PL47.C
@@ -7,11 +7,11 @@ scanf("%d%d%d",&n,&i,&j);
 k=n+i+j;
 if(k==180)
 {
-printf("\n they can be interior angles of a triangle");	
+printf("\n yes");	
 }
 else
 {
-printf("\n they can't be interior angles of a triangle");	
+printf("\n no");	
 }
 return(0);
 }
