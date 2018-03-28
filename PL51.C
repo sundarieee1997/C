a63dab935b0040ee568334e5eb709c3ab73bbdcd
sundarieee1997 <35525587+sundarieee1997@users.noreply.github.com>
2018-03-28 11:00:41 +0000

    PL51.C
    program to find the 2nd smallest number

---
+++ PL51.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int i,j,s,k,a[100];
+printf("enter the number");
+scanf("%d",&s);
+printf("\n enter the elements");
+for(i=0;i<s;i++)
+{
+scanf("%d",&k);
+}
+for(i=0;i<s;i++)
+{
+for(j=i+1;j<s;j++)
+{
+if(a[i]>a[j])
+{
+k=a[i];
+a[i]=a[j];
+a[j]=k;
+}
+}
+}
+printf("\n the 2nd smallest number is:%d",a[i]);
+return 0;
+}
